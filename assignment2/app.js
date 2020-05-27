a08db86ec2f8b807cf3700e83c523c033f983431
Surendar <suredirector1998@gmail.com>
2020-05-27 15:19:07 +0530

    Assignment2

---
+++ assignment2/app.js
@@ -0,0 +1,76 @@
+(function(){
+	angular.module('ShoppingApp',[])
+	.controller('ToBuyController',ListController)
+	.controller('AlreadyBoughtController',Bought)
+	.service('ShoppingListCheckOffService',AddItems);
+
+	Bought.$inject = ['ShoppingListCheckOffService'];
+	function Bought(ShoppingListCheckOffService){
+		var b = this;
+		b.list = [];
+		b.show = function(){
+			b.list = ShoppingListCheckOffService.bou();
+		}
+		b.check = function(){
+			b.list = ShoppingListCheckOffService.bou();
+			if(b.list.length === 0){
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+
+
+	ListController.$inject = ['ShoppingListCheckOffService'];
+	function ListController(ShoppingListCheckOffService){
+		console.log("hi");
+		var shop = this;
+		shop.item = "";
+		shop.quantity = "";
+		shop.list = [];
+		shop.add = function(){
+			ShoppingListCheckOffService.add(shop.item,shop.quantity);
+			shop.list = ShoppingListCheckOffService.show();
+		}
+		shop.bought = function(index){
+			ShoppingListCheckOffService.bought(index);
+		}
+		shop.check = function(){
+			shop.list = ShoppingListCheckOffService.show();
+			if(shop.list.length === 0){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+	}
+
+	function AddItems(){
+		var service = this;
+		var items = [];
+		var boughtItems = [];
+		service.add = function(item,quantity){
+			var lis = {
+				name : item,
+				quantity : quantity
+			};
+			items.push(lis);
+		}
+
+		service.show = function(){
+			return items;
+		}
+		service.bought = function(index){
+			boughtItems.push(items[index]);
+			console.log(boughtItems[0].name+" "+boughtItems[0].quantity);
+			items.splice(index,1);
+
+		}
+		service.bou = function(){
+			return boughtItems;
+		}
+	}
+})();
